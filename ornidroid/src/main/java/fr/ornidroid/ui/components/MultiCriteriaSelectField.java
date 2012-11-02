46a3c5e21943acbf09c84d5ca0b5aff4b229cb5e
giletvin <giletvin@gmail.com>
2012-11-02 13:45:27 +0100

    * #6 refactoring sur l'activité  	* création d'un nouveau composant custom pour gérer les champs selects dans le formulaire de recherche

---
+++ ornidroid/src/main/java/fr/ornidroid/ui/components/MultiCriteriaSelectField.java
@@ -0,0 +1,99 @@
+package fr.ornidroid.ui.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import fr.ornidroid.R;
+
+/**
+ * The Class MultiCriteriaSelectField.
+ * http://kevindion.com/2011/01/custom-xml-attributes-for-android-widgets/
+ */
+public class MultiCriteriaSelectField extends LinearLayout {
+
+	/** The data adapter. */
+	private ArrayAdapter<String> dataAdapter;
+
+	/** The spinner. */
+	private final Spinner spinner;
+
+	/** The text view. */
+	private final TextView textView;
+
+	/**
+	 * Instantiates a new multi criteria select field.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param attrs
+	 *            the attrs
+	 */
+	public MultiCriteriaSelectField(final Context context,
+			final AttributeSet attrs) {
+		super(context, attrs);
+
+		this.textView = new TextView(context);
+		this.spinner = new Spinner(context);
+		this.addView(this.textView);
+		this.addView(this.spinner);
+		this.textView.setOnClickListener(new OnClickListener() {
+			public void onClick(final View v) {
+				if (MultiCriteriaSelectField.this.spinner.getVisibility() == View.VISIBLE) {
+					MultiCriteriaSelectField.this.spinner
+							.setVisibility(View.GONE);
+				} else {
+					MultiCriteriaSelectField.this.spinner
+							.setVisibility(View.VISIBLE);
+				}
+			}
+		});
+		final TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.MultiCriteriaSelectField);
+
+		final int N = a.getIndexCount();
+		for (int i = 0; i < N; ++i) {
+			final int attr = a.getIndex(i);
+			switch (attr) {
+			case R.styleable.MultiCriteriaSelectField_text:
+				final String myText = a.getString(attr);
+				this.textView.setText(myText);
+				break;
+			case R.styleable.MultiCriteriaSelectField_spinnerPrompt:
+				final String spinnerPrompt = a.getString(attr);
+				this.spinner.setPrompt(spinnerPrompt);
+				break;
+
+			}
+		}
+		a.recycle();
+
+	}
+
+	/**
+	 * Gets the spinner.
+	 * 
+	 * @return the spinner
+	 */
+	public Spinner getSpinner() {
+		return this.spinner;
+	}
+
+	/**
+	 * Sets the data adapter.
+	 * 
+	 * @param dataAdapter
+	 *            the new data adapter
+	 */
+	public void setDataAdapter(final ArrayAdapter<String> dataAdapter) {
+		this.dataAdapter = dataAdapter;
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		this.spinner.setAdapter(dataAdapter);
+	}
+
+}

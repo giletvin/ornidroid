c81ca241086f2f5328e979202df3b051eded4f7c
giletvin <giletvin@gmail.com>
2012-11-13 22:27:49 +0100

    ne plus ignorer le sql de formes de beaks

---
+++ ornidroid_database/insert_data_table_beak_form.sql
@@ -0,0 +1,12 @@
+-- insert des types de bec en français.
+-- les id sont en dur ! Cela a un impact sur la classe BeakFormsHelper pour déterminer l'icone à afficher
+
+INSERT INTO beak_form(id,name,lang) VALUES(1,"autres becs droits",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(2,"épais et court",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(3,"autre",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(4,"courbé",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(5,"droit et long",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(6,"crochu",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(7,"fin et court",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(8,"canard",'fr');
+INSERT INTO beak_form(id,name,lang) VALUES(9,"mouette",'fr');

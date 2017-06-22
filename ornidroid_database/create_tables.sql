fb57184852900d732251b903ce8f7c3caa5d9b15
giletvin <giletvin@gmail.com>
2017-06-22 21:21:26 +0200

    * mise à jour des release notes

--- ornidroid_database/create_tables.sql
+++ ornidroid_database/create_tables.sql
@@ -108,10 +108,13 @@ insert into application_info(id,key,value,date,comments) VALUES(7,"ornidroid_ver
 
 create table release_notes(id integer,version_code integer,date,comments_de,comments_en,comments_fr,read integer);
 
--- release 11.2.0 version 29
-insert into release_notes(id,version_code,date,comments_en,comments_de,comments_fr,read) VALUES(5,29,"31/01/2016","","","",0);
+-- release 11.3.0 version 30
+insert into release_notes(id,version_code,date,comments_en,comments_de,comments_fr,read) VALUES(6,30,"22/06/2017","","","",0);
 
-update release_notes set comments_fr=" * Version 11.2.0 (31/01/2016)
+update release_notes set comments_fr=" * Version 11.3.0 (22/06/2017)
+Evolutions : corrections dans la base de données, nombreuses nouvelles photos en ligne : pensez à activer l''option de vérification de mises à jour pour avoir des notifications !
+
+ * Version 11.2.0 (31/01/2016)
 Evolutions : 
  - ajout des traductions en catalan, basque et galicien. Merci à Maria pour son aide !
 
@@ -145,9 +148,12 @@ Evolutions :
  - refonte de l''écran de détail des oiseaux
  - téléchargement des packages complets d''images, sons et pages wikipedia depuis l''application
  - améliorations de la base de données de sons : ajout de nombreux fichiers mp3
-" where version_code=29;
+" where version_code=30;
 
-update release_notes set comments_en=" * Version 11.2.0 (01/31/2016)
+update release_notes set comments_en=" * Version 11.3.0 (06/22/2017)
+Evolutions : few fixes in the db, new pictures online : you can set the check for updates option to get them.
+
+ * Version 11.2.0 (01/31/2016)
 New features:
  - database in catalan, basque and galician, thanks to Maria !
 
@@ -184,43 +190,4 @@ New features :
  - More mp3 files from Xeno Canto available
  - Release notes screen
 
-" where version_code=29;
-
-update release_notes set comments_de=" * Version 11.2.0 (01/31/2016)
-New features:
- - database in catalan, basque and galician, thanks to Maria !
-
- * Version 11.1.0 (07/20/2015)
-New features:
- - enhancements on the zoomed images screen
- - few fixes in the database
- - enhancements in the multi criteria search
-
- * Version 11.0.0 (01/23/2015)
-
-New features : 
- - database in serbian and slovenian
-
-Bug fixes :
- - Complete code refactoring : better handling of download processes, better handling of screen rotations, ... 
-
- * Version 10.0.0 (10/15/2014)
-
-New features : 
- - enhancements on the mp3 player : progress bar, played item is now highlighted
-
- * Version 9.1.0 (09/23/2014)
-
-New features : 
- - database in turkish
-
- * Version 9.0.0 (09/09/2014)
-
-New features : 
- - Wikipedia files embedded in the app
- - redesign of the main screen
- - download of full packages of images, sounds or wikipedia files from the app
- - More mp3 files from Xeno Canto available
- - Release notes screen
-
-" where version_code=27;
+" where version_code=30;
